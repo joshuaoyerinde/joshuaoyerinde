5e4ffc4a4895cc61295a70790d9ece125244ba66
joshuaoyerinde <oyerindejoshua133@gamil.com>
2020-02-03 06:16:08 -0800

    the work is ready

---
+++ cont.php
@@ -0,0 +1,69 @@
+
+<style type="text/css">
+.inner{
+    overflow: hidden;
+}
+.inner img{
+    transition: all 1.5s ease;
+}
+.inner:hover img{
+    transform: scale(2);
+}
+
+</style>
+<div class="" id="about">
+	<div class="text-center">
+		<h4>THE BAND</h4>
+		<p>We love music</p>
+	</div>
+	<div class="justify-content-center w3-justify">
+		<div>
+			We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+		</div>
+	</div>
+	<div class="row justify-content-center mt-3">
+		<div class="col-md-4 mt-4">
+			<div class="card shadow">
+				<div class="inner">
+				<a target="_blank" href="gdimg/cinim (6).jpg">
+				<img src="gdimg/cinim (6).jpg" class="card-img-top" alt="...">
+				</a>
+				
+			   </div>
+			  <div class="card-body text-center">
+			    <h5 class="card-title">Design</h5>
+			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+			    Some quick example text to build on the card title and make up</p>
+			    <a href="#" class="btn btn-warning">Learn More....</a>
+			  </div>
+			</div>
+		</div>
+		<div class="col-md-4 mt-4">
+			<div class="card shadow">
+				<div class="inner">
+					 <img src="gdimg/frame 4.jpg" class="card-img-top" alt="...">
+				</div>
+			  <div class="card-body text-center">
+			    <h5 class="card-title">Robot Egine.</h5>
+			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+			    Some quick example text to build on the card title and make up</p>
+			    <a href="#" class="btn btn-warning">Learn More....</a>
+			  </div>
+			</div>
+		</div>
+		<div class="col-md-4 mt-4">
+			<div class="card shadow">
+				<div class="inner">
+					<img src="gdimg/notebook.jpg" class="card-img-top" alt="...">
+				</div>
+			  <div class="card-body text-center">
+			    <h5 class="card-title">UI/UX</h5>
+			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
+			    Some quick example text to build on the card title and make up</p>
+			    <a href="#" class="btn btn-warning">Learn More....</a>
+			  </div>
+			</div>
+		</div>
+			
+	</div>
+</div>
