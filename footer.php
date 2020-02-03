3e6a3ca9a449963dbdcc960c128acb4b33586483
joshuaoyerinde <oyerindejoshua133@gamil.com>
2020-02-03 06:19:18 -0800

    footer is working nice

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="w3-container w3-center w3-margin-top bg-light mt-3" id="footer">
+  <p class="mt-3 natem">Find me on social media.</p>
+  <div class="btn btn-primary"><i class="fa fa-facebook w3-hover-opacity"></i></div>
+  <div class="btn btn-primary"><i class="fa fa-instagram"></i></div>
+  <div class="btn btn-success"><i class="fa fa-whatsapp"></i></div>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <div class="btn btn-info"><i class="fa fa-twitter"></i></div>
+  <p class="natem mt-2">Powered by<a href="default.html" target="_blank">Josh Man</a></p>
+</footer>
