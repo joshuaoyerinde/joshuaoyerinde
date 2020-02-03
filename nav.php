4a200d0ba4cc3a2487c5999b2f20e751080c761d
joshuaoyerinde <oyerindejoshua133@gamil.com>
2020-02-03 06:20:42 -0800

    nav is added to to the index

---
+++ nav.php
@@ -0,0 +1,20 @@
+
+<nav class="navbar navbar-expand-md bg-light navbar-light shadow fixed">
+    <div class="container">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <i class="fa fa-bars navbar-toggler" data-target="#collapsibleNavbar" data-toggle="collapse"></i>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact Us</a>
+          </li>    
+        </ul>
+      </div>  
+  </div>
+  </nav>
